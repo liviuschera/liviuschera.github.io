2808ce443d3c2dae03cf368ca842d21af1913c11
Liviu Schera <liviu_schera@epam.com>
2025-02-27 15:38:51 +0200

    refactor(data): Convert portfolio section from HTML to JavaScript data structure
    - Create new portfolioData.js file to store project information
    - Add portfolio renderer to main.js
    - Update HTML to use dynamic rendering for portfolio section
    - Maintain hover effects functionality
    - Improve maintainability by centralizing portfolio information

---
+++ js/data/portfolio.js
@@ -0,0 +1,49 @@
+export const portfolioData = [
+    {
+        title: "The Gym Outlet Online Shop",
+        description: "E-commerce web site built With React, Firebase",
+        tags: ["React", "Firebase"],
+        imageUrl: "./img/gym-outlet.png",
+        imageAlt: "Screenshot of the Gym Outlet Shop",
+        projectUrl: "https://gym-outlet.netlify.app/",
+    },
+    {
+        title: "Gym Pass (Under Construction)",
+        description:
+            "Gym management app built with React, React query, react router, styled components and Supabase.",
+        tags: ["React", "Supabase", "styled componens", "react router"],
+        imageUrl: "./img/gympass.png",
+        imageAlt: "Screenshot of Noctis theme",
+        projectUrl: "https://gym-pass-luap.onrender.com/",
+        className: "noctis",
+    },
+    {
+        title: "Noctis theme for VS Code",
+        description:
+            "Designed and coded a theme generator for VS Code using NodeJS and published it on the Visual Studio Code Marketplace.",
+        tags: ["JavaScript", "NodeJS"],
+        imageUrl: "./img/noctis.png",
+        imageAlt: "Screenshot of Noctis theme",
+        projectUrl:
+            "https://marketplace.visualstudio.com/items?itemName=liviuschera.noctis",
+        className: "noctis",
+    },
+    {
+        title: "Mundus",
+        description:
+            "A React that integrates with the REST Countries API, Google Maps and Pixabay APIs.",
+        tags: ["React", "React Hooks", "React Router", "APIs"],
+        imageUrl: "./img/mundus.png",
+        imageAlt: "Screenshot of Mundus App",
+        projectUrl: "https://mundus.netlify.app/",
+    },
+    {
+        title: "Oil Discovery",
+        description:
+            "I have built a custom MVC Framework from scratch and complete user authentication using PHP (OOP & PDO), SASS and HTML5.",
+        tags: ["HTML5", "CSS3", "JavaScript", "PHP"],
+        imageUrl: "./img/oildiscovery.png",
+        imageAlt: "Screenshot of Oildiscovery site",
+        projectUrl: "https://github.com/liviuschera/oildiscovery",
+    },
+];
